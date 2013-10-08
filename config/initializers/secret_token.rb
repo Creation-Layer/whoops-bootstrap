af6399bae44c9c0052d67f223f89f5c75192684f
John Hanley <jhanleysf@gmail.com>
2013-10-08 02:15:33 -0700

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WhoopsServer::Application.config.secret_token = '3a46eb92305e4b338cec8baecd8d97a9034c99fc1b6b3a9a8632a71cecac050b7c47524b183ef04793dbd50a06610676ac06fc772ba2c679cb555fb9095b65bb'
+WhoopsServer::Application.config.secret_token = '3a46eb92305e4b338cec8baecd8vfvfdbfdb9a8632a71cecac050b7c47524b183ef04793dbd50a06610676ac06fc772ba2c679cb555fb9095b65bb'
